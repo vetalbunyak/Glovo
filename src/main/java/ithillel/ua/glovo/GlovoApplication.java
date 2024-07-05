05bc101c2dc0ffa4f07aaa069e1b9e6c39e0070c
Vitaliy <vbnyak@gmail.com>
2024-07-05 15:21:15 +0300

    Initial commit

---
+++ src/main/java/ithillel/ua/glovo/GlovoApplication.java
@@ -0,0 +1,13 @@
+package ithillel.ua.glovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlovoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlovoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ithillel/ua/glovo/GlovoApplicationTests.java
@@ -0,0 +1,13 @@
+package ithillel.ua.glovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlovoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
